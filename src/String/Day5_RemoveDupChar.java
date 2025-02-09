70c4f7ddb94c5887b298e9051579ca149027dca1
강보령 <kangbr16@naver.com>
2025-02-09 21:36:05 +0900

    Update: 강의 코드 추가'

--- src/String/Day5_RemoveDupChar.java
+++ src/String/Day5_RemoveDupChar.java
@@ -15,10 +15,16 @@ public class Day5_RemoveDupChar {
     public String solution(String str) {
         ArrayList<String> strList = new ArrayList<>();
         for (String s : str.split("")) {
-            if(strList.contains(s)) continue;
-            strList.add(s);
+            if(!strList.contains(s)) strList.add(s);
         }
-
         return String.join("", strList);
+    }
+
+    public String solution_lecture(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i) answer += str.charAt(i);
+        }
+        return answer;
     }
 }
