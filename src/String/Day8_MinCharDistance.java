9a32c05a6c748b4418e8f476cfaaa24b817a7971
강보령 <boryung@boryung-MacBookPro.local>
2025-02-27 21:13:03 +0900

    Solve: Day8 - 10.가장 짧은 문자거리

--- src/String/Day8_MinCharDistance.java
+++ src/String/Day8_MinCharDistance.java
@@ -1,4 +1,38 @@
 package String;
 
+import java.util.Scanner;
+
 public class Day8_MinCharDistance {
+
+    public static void main(String[] args) {
+        Day8_MinCharDistance T = new Day8_MinCharDistance();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        char t = sc.next().charAt(0);
+        for ( int answer : T.solution(input, t)) {
+            System.out.print(answer + " ");
+        }
+    }
+
+    public int[] solution(String str, char t) {
+        int[] intArr = new int[str.length()];
+
+        int dis = 1000;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == t) dis = 0;
+            intArr[i] = dis;
+            dis++;
+        }
+
+        for (int i = str.length() - 1; i > -1; i--) {
+            if (str.charAt(i) == t) {
+                dis = 0;
+                dis++;
+                continue;
+            }
+            intArr[i] = Math.min(intArr[i], dis);
+            dis++;
+        }
+        return intArr;
+    }
 }
